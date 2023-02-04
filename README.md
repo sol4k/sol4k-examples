14ba2c533dd5cc82a133c7ae3c5ae8f18cdde077
Sasha Shpota <5640984+Shpota@users.noreply.github.com>
2023-02-04 20:29:10 +0300

    Sending transactions using sol4k in Java

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# sol4k demo 
-A demo how to use sol4k
+# sol4k demo [![Build](https://github.com/sol4k/sol4k-demo/actions/workflows/build.yml/badge.svg)](https://github.com/sol4k/sol4k-demo/actions/workflows/build.yml)
+
+Sending transactions using sol4k in Java.
+
+Imports:
+```java
+import org.bitcoinj.core.Base58;
+import org.sol4k.Connection;
+import org.sol4k.Keypair;
+import org.sol4k.PublicKey;
+import org.sol4k.Transaction;
+import org.sol4k.TransferInstruction;
+```
+
+Sending 1000 Lamport from one account to another: 
+```java
+var connection = new Connection("https://api.devnet.solana.com");
+var latestBlockhash = connection.getLatestBlockhash();
+var sender = Keypair.fromSecretKey(Base58.decode("2WGcYY..."));
+ var receiver = new PublicKey("DxPv2QMA5cWR5Xfg7tXr5YtJ1EEStg5Kiag9HhkY1mSx");
+var instruction = new TransferInstruction(sender.getPublicKey(), receiver, 1000);
+var transaction = new Transaction(
+        latestBlockhash.getBlockhash(),
+        instruction,
+        sender.getPublicKey()
+);
+transaction.sign(sender);
+var signature = connection.sendTransaction(transaction);
+System.out.println("Signature " + signature);
+```
--- src/main/java/org/sol4kdemo/Main.java
+++ src/main/java/org/sol4kdemo/Main.java
@@ -1,7 +1,27 @@
 package org.sol4kdemo;
 
+import org.bitcoinj.core.Base58;
+import org.sol4k.Connection;
+import org.sol4k.Keypair;
+import org.sol4k.PublicKey;
+import org.sol4k.Transaction;
+import org.sol4k.TransferInstruction;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // send 1000 Lamport from one address to another
+        var connection = new Connection("https://api.devnet.solana.com");
+        var latestBlockhash = connection.getLatestBlockhash();
+        var sender = Keypair.fromSecretKey(Base58.decode("2WGcYYau2gLu2DUq68SxxXQmCgi77n8hFqqLNbNyg6Xfh2m3tvg8LF5Lgh69CFDux41LUKV1ak1ERHUqiBZnyshz"));
+         var receiver = new PublicKey("DxPv2QMA5cWR5Xfg7tXr5YtJ1EEStg5Kiag9HhkY1mSx");
+        var instruction = new TransferInstruction(sender.getPublicKey(), receiver, 1000);
+        var transaction = new Transaction(
+                latestBlockhash.getBlockhash(),
+                instruction,
+                sender.getPublicKey()
+        );
+        transaction.sign(sender);
+        var signature = connection.sendTransaction(transaction);
+        System.out.println("Signature " + signature);
     }
 }
