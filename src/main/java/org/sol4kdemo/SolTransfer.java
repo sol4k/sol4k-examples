a3f0691644c2b855f5c6141dbfb60be5f0c39860
Sasha Shpota <5640984+Shpota@users.noreply.github.com>
2023-03-24 20:06:01 +0300

    Use the updated version of getLatestBlockhash (#8)

--- src/main/java/org/sol4kdemo/SolTransfer.java
+++ src/main/java/org/sol4kdemo/SolTransfer.java
@@ -10,13 +10,13 @@ import org.sol4k.instruction.TransferInstruction;
 public class SolTransfer {
     public static void main(String[] args) {
         var connection = new Connection("https://api.devnet.solana.com");
-        var latestBlockhash = connection.getLatestBlockhash();
+        var blockhash = connection.getLatestBlockhash();
         // fund this account in case it is empty
         var sender = Keypair.fromSecretKey(Base58.decode("2WGcYYau2gLu2DUq68SxxXQmCgi77n8hFqqLNbNyg6Xfh2m3tvg8LF5Lgh69CFDux41LUKV1ak1ERHUqiBZnyshz"));
         var receiver = new PublicKey("DxPv2QMA5cWR5Xfg7tXr5YtJ1EEStg5Kiag9HhkY1mSx");
         var instruction = new TransferInstruction(sender.getPublicKey(), receiver, 1000);
         var transaction = new Transaction(
-                latestBlockhash.getBlockhash(),
+                blockhash,
                 instruction,
                 sender.getPublicKey()
         );
