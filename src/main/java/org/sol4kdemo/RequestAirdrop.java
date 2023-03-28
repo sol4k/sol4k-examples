aa2d765ed5b290811b50b4308487a4a113a76c4b
Sasha Shpota <5640984+Shpota@users.noreply.github.com>
2023-03-28 21:51:06 +0300

    Use enum for the devnet public RPC URL (#9)

--- src/main/java/org/sol4kdemo/RequestAirdrop.java
+++ src/main/java/org/sol4kdemo/RequestAirdrop.java
@@ -2,10 +2,11 @@ package org.sol4kdemo;
 
 import org.sol4k.Connection;
 import org.sol4k.PublicKey;
+import org.sol4k.RpcUrl;
 
 public class RequestAirdrop {
     public static void main(String[] args) {
-        var connection = new Connection("https://api.devnet.solana.com");
+        var connection = new Connection(RpcUrl.DEVNET);
         var wallet = new PublicKey("2mjCPF5ABp6CVAYHd5azz5A8cKQUqqvuiT5NJ4wpAjCc");
         // note: this call can be rate-limited by the RPC node and fail from time to time
         var signature = connection.requestAirdrop(wallet, 1_000_000_000);
